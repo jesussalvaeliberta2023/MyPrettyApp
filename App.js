9904f8c856fa4aee606e842beb0d59d9ecab0e61
Wellington <wellington.gts21@gmail.com>
2024-02-21 12:29:31 -0300

    Fiz tudo graças a Deus

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.secondaryContainer}>
+        <Text style={styles.myName} >Wellington de Oliveira</Text>
+        <Text style={styles.myNumber} >34</Text>
+        <Text style={styles.classNumber} >07</Text>
+        <Text style={styles.myThing} >#VouComJesus</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  secondaryContainer: {
+    flex: 1 ,
+    width: '100%',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+
+  myName: {
+    color: 'blue',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'end',
+  },
+
+  myNumber: {
+    borderWidth: 1,
+    borderColor: 'purple',
+    fontSize: 25,
+    fontStyle: 'italic',
+    textAlign: 'start',
+  },
+
+  classNumber: {
+    backgroundColor: 'pink',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+
+  myThing: {
+    borderWidth: 2,
+    color: 'white',
+    borderColor: 'grey',
+    backgroundColor: 'orange',
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
 });
